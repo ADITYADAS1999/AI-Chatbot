cbe8c02fc7f2da7817fbcfdb7e295a07366b5cdd
ADITYA DAS <58718316+ADITYADAS1999@users.noreply.github.com>
2022-07-17 20:38:35 +0530

    Update app.py

--- flask-chatterbot-master/app.py
+++ flask-chatterbot-master/app.py
@@ -4,18 +4,18 @@ from chatterbot.trainers import ChatterBotCorpusTrainer
 
 app = Flask(__name__)                               # Flask constructor takes the name of current module (__name__) as argument. The route() function of the Flask class is a decorator, which tells the application which URL should call the associated function.
 
-english_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")  # The SQLStorageAdapter allows ChatterBot to store conversation data in any database supported by the SQL Alchemy ORM. 
-trainer = ChatterBotCorpusTrainer(english_bot)  # This is a corpus of dialog data that is included in the chatterbot module. Additional information about the chatterbot-corpus module can be found in the ChatterBot Corpus Documentation.
+my_bot = ChatBot("Chatterbot", storage_adapter="chatterbot.storage.SQLStorageAdapter")  # The SQLStorageAdapter allows ChatterBot to store conversation data in any database supported by the SQL Alchemy ORM. 
+trainer = ChatterBotCorpusTrainer(my_bot)  # This is a corpus of dialog data that is included in the chatterbot module. Additional information about the chatterbot-corpus module can be found in the ChatterBot Corpus Documentation.
 trainer.train("chatterbot.corpus.english")
 
 @app.route("/")                             # This is called the decorator fucntion in python. This essentially wrap our functions inside the app.route fuction.
-def home():
+def my_home():
     return render_template("index.html")
 
 @app.route("/get")
-def get_bot_response():
+def get_chat_bot_response():
     userText = request.args.get('msg')
-    return str(english_bot.get_response(userText))
+    return str(my_bot.get_response(userText))
 
 
 if __name__ == "__main__":                     # This application run by the python interpreter gives a name variable that set the actual name of the program file.
